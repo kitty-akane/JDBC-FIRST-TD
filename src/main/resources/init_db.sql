0db0de18c5140913302fe51d1ec366136a1d5174
sitraka <sitraka7011@gmail.com>
2025-12-10 18:09:59 +0300

    updated: init db

--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -2,8 +2,6 @@ CREATE DATABASE product_management_db;
 
 CREATE USER product_manager_user WITH PASSWORD '123456';
 
-CREATE DATABASE product_management_db;
-
 GRANT CONNECT ON DATABASE product_management_db TO product_manager_user;
 
 GRANT USAGE, CREATE ON SCHEMA public TO product_manager_user;
